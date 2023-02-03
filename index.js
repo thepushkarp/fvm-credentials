e89495fa28413e68b7f6a40414d5fa19d474c132
Pushkar Patel <42088801+thepushkarp@users.noreply.github.com>
2023-02-04 04:38:12 +0530

    Modify exports from index

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-export { getLocationHash, genCID, create_DID, hash, register_DID } from './main';
+import { getLocationHash, genCID, create_DID, hash, register_DID } from './main';
+
+module.exports = {
+    getLocationHash,
+    genCID,
+    create_DID,
+    hash,
+    register_DID,
+};
